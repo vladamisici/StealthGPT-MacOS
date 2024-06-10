ffa32c01bb73288f39dfd16db648deaf25170cb0
BlackGI6 <vladamisici@yahoo.com>
2024-06-10 21:23:56 +0300

    Initial Commit

---
+++ StealthGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StealthGPT
+//
+//  Created by Vlada Misici on 10.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StealthGPT/StealthGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  StealthGPTApp.swift
+//  StealthGPT
+//
+//  Created by Vlada Misici on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct StealthGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
